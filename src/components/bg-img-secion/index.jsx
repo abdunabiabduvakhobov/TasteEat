b0608686b4df235c74393a9a5907570d5700f737
azimjonxatamov <azimjonxatamov6@gmail.com>
2024-11-21 12:59:16 +0500

    bg img parh changed

--- src/components/bg-img-secion/index.jsx
+++ src/components/bg-img-secion/index.jsx
@@ -2,7 +2,7 @@
 
 export const BgImgSecion = () => {
   return (
-    <section className="h-screen bg-[url('./bgimgsecion.png')] bg-no-repeat bg-cover bg-center bg-fixed">
+    <section className="h-screen bg-[url('../bgimgsecion.png')] bg-no-repeat bg-cover bg-center bg-fixed">
 
     </section>
   )
